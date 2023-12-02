33b564b5973d470f7d11691b07e2591daa5a37ed
Lenovo <liubchenkalexander@gmail.com>
2023-12-02 17:20:45 +0200

    Add Pagination, Sorting, and Swagger to the existing controllers

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -2,9 +2,12 @@ package controller;
 
 import dto.BookDto;
 import dto.CreateBookRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mapper.BookMapper;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import service.BookService;
 
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/books")
@@ -25,28 +29,33 @@ public class BookController {
     private final BookMapper bookMapper;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books", description = "Get a list of all available books")
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a book by id", description = "Gets a book by id")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
     @PostMapping
+    @Operation(summary = "Create a new book", description = "Creates a new book")
     public BookDto createBook(@RequestBody CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a book by id", description = "Delete a book by id in database")
     public void deleteBook(@PathVariable Long id) {
         bookService.deleteById(id);
     }
 
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PutMapping("/{id}")
+    @Operation(summary = "Update a book by id", description = "Update a book by id in database")
     public void updateBook(@PathVariable Long id, @RequestBody CreateBookRequestDto requestDto) {
         bookService.update(id, bookMapper.toModel(requestDto));
     }
--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -4,11 +4,12 @@ import dto.BookDto;
 import dto.CreateBookRequestDto;
 import java.util.List;
 import model.Book;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto getBookById(Long id);
 
