83a2e8ef1fc284ad1fad7afba5d81c3e7c0010f6
Lenovo <liubchenkalexander@gmail.com>
2023-12-14 09:58:21 +0200

    whitespace add

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -17,13 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true, nullable=false)
+    @Column(unique = true, nullable = false)
     private String email;
-    @Column(unique = true, nullable=false)
+    @Column(unique = true, nullable = false)
     private String password;
-    @Column(unique = true, nullable=false)
+    @Column(unique = true, nullable = false)
     private String firstName;
-    @Column(unique = true, nullable=false)
+    @Column(unique = true, nullable = false)
     private String lastName;
     private String shippingAddress;
 }
