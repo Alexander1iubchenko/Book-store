2ccf530cb8215412b81853586f295b93cafa0f3d
Lenovo <liubchenkalexander@gmail.com>
2023-12-26 19:57:11 +0200

    order

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -6,6 +6,7 @@ import dto.user.UserRegistrationRequestDto;
 import dto.user.UserResponseDto;
 import exception.RegistrationException;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import security.AuthenticationService;
 import service.UserService;
-
-import javax.validation.Valid;
 
 @Tag(name = "User management", description = "Endpoints for managing users")
 @RequiredArgsConstructor
