2a167d7945f6b7795582ad9cf5042dfb631e6894
Lenovo <liubchenkalexander@gmail.com>
2024-01-08 23:08:20 +0200

    add .env, docker-compose.yml, Dockerfile

--- src/test/java/application/BookStoreApplicationTests.java
+++ src/test/java/application/BookStoreApplicationTests.java
@@ -1,13 +1,8 @@
 package application;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookStoreApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
