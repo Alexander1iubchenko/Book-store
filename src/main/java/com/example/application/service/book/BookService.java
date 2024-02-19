1c49fa79a2b4b181cbb223255d18163fede722c2
Lenovo <liubchenkalexander@gmail.com>
2024-02-19 17:15:09 +0200

    add BookRepository, BookServiceImpl and BookService

--- src/main/java/com/example/application/service/book/BookService.java
+++ src/main/java/com/example/application/service/book/BookService.java
@@ -2,6 +2,7 @@ package com.example.application.service.book;
 
 import com.example.application.dto.book.BookDto;
 import com.example.application.dto.book.BookDtoWithoutCategoriesIds;
+import com.example.application.dto.book.BookSearchParametersDto;
 import com.example.application.dto.book.CreateBookRequestDto;
 import com.example.application.model.Book;
 import java.util.List;
@@ -19,4 +20,6 @@ public interface BookService {
     void update(Long id, Book book);
 
     List<BookDtoWithoutCategoriesIds> getBooksByCategoryId(Long id, Pageable pageable);
+
+    List<BookDto> search(BookSearchParametersDto searchParameters, Pageable pageable);
 }
